a307413eaa969643abf6bbf86b19ae41a1e6796e
mahdiyebahmani <119834782+mahdiyebahmani@users.noreply.github.com>
2024-02-09 16:11:34 +0330

    file completed

--- tehran.cpp
+++ tehran.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void Tehran::CreatCity()
 {
     std::ifstream Taxi_in ("Taxi.txt");
+    std::ifstream Bus_in ("Bus.txt");
     map <string,int> find_index;
     Path temp;
     string origin;
@@ -17,9 +18,10 @@ void Tehran::CreatCity()
 
     if(Taxi_in.is_open())
     {
-        temp.Set_taxi(1);
+       
         while(Taxi_in >> origin_index >> origin >> destination_index >> destination >> distance)
         {
+            temp.Set_taxi(1);
             temp.Set_origin(origin, origin_index);
             temp.Set_destination(destination, destination_index);
             temp.Set_distance(distance);
@@ -40,6 +42,34 @@ void Tehran::CreatCity()
 
     else 
     cout << "Error occured while opening file"<<endl;
+
+
+    if(Bus_in.is_open())
+    {
+        while(Bus_in >> origin_index >> origin >> destination_index >> destination >> distance)
+        {
+            temp.Set_Bus(1);
+            temp.Set_origin(origin, origin_index);
+            temp.Set_destination(destination, destination_index);
+            temp.Set_distance(distance);
+            tehran[origin_index][destination_index] = temp;
+            tehran[destination_index][origin_index] = temp;
+
+            if (find_index.find(origin) == find_index.end())
+            {
+                find_index.insert({origin, origin_index});
+            }
+
+            if (find_index.find(destination) == find_index.end())
+            {
+                find_index.insert({destination, destination_index});
+            }
+        }
+    }
+
+    else 
+    cout << "Error occured while opening file"<<endl;
+
 }
 
 int main()
