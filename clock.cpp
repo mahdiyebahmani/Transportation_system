e599e7b648c54a28350a362554642c9bd710fbec
nimatrixx <www.nimatrix@gmail.com>
2024-03-01 23:47:52 +0330

    add Clock class

---
+++ clock.cpp
@@ -0,0 +1,126 @@
+#include "clock.h"
+using namespace std;
+
+Clock::Clock() : hour{0}, minute{0} {}
+
+Clock::Clock(int minutes)
+{
+    setFromMinute(minutes);
+}
+
+void Clock::setFromMinute(int minutes)
+{
+    int hour = 0;
+    while(minutes >= 60)
+    {
+        hour++;
+        minutes -= 60;
+
+        if(hour >= 24)
+            hour = 0;
+    }
+
+    minute = minutes;
+}
+
+void Clock::setFromString(string clock)
+{
+    if(clock.length() > 5)
+        throw ("invalid clock");
+
+    int tmp = -1;
+    bool hourAssigned = 0;
+    for(char ch : clock)
+    {
+        if(ch == ':')
+        {
+            setHour(tmp);
+            tmp = -1;
+        }
+        else
+        {
+            int num = int(ch) - 48;
+            if(num < 9 && num >= 0)
+            {
+                if(tmp != -1)
+                    tmp += num;
+                else
+                    tmp += num * 10 + 1;
+            }
+        }
+    }
+    setMinute(tmp);
+}
+
+void Clock::setMinute(int minute)
+{
+    if(minute >= 60 || minute < 0)
+        this->minute = 0;
+    else
+        this->minute = minute;
+}
+
+void Clock::setHour(int hour)
+{
+    if(hour >= 24 || hour < 0)
+        this->hour = 0;
+    else
+        this->hour = hour;
+}
+
+void Clock::addHour(int hour)
+{
+    if(hour < 0)
+        return;
+
+    this->hour = (this->hour + hour) % 24;
+}
+
+void Clock::addMinute(int minute)
+{
+    if(minute < 0)
+        return;
+
+    this->minute = (this->minute + minute) % 60;
+    addHour(int(minute / 60));
+}
+
+int Clock::getHour()
+{
+    return hour;
+}
+
+int Clock::getMinute()
+{
+    return minute;
+}
+
+string Clock::getHourInString()
+{
+    string s;
+    if(hour < 10)
+        s += '0';
+    s += to_string(hour);
+
+    return s;
+}
+
+string Clock::getMinuteInString()
+{
+    string s;
+    if(minute < 10)
+        s += '0';
+    s += to_string(minute);
+
+    return s;
+}
+
+string Clock::getClockInString()
+{
+    string s;
+    s += getHourInString();
+    s += ':';
+    s += getMinuteInString();
+
+    return s;
+}
---
+++ clock.h
@@ -0,0 +1,30 @@
+#ifndef CLOCK
+#define CLOCK
+
+#include <string>
+
+class Clock
+{
+    public:
+    Clock();
+    Clock(int);
+    void setFromMinute(int);
+    void setFromString(std::string);
+    void setMinute(int);
+    void setHour(int);
+    void addHour(int);
+    void addMinute(int);
+
+    int getHour();
+    int getMinute();
+    int getInMinute();
+    std::string getHourInString();
+    std::string getMinuteInString();
+    std::string getClockInString();
+
+    private:
+    int hour;
+    int minute;
+};
+
+#endif
